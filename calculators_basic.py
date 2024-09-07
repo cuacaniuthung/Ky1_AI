12384d5f6929c9083bb3aa928eb0d74becb3a9d1
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2024-09-07 21:34:15 +0700

    Add files via upload

---
+++ calculators_basic.py
@@ -0,0 +1,23 @@
+#Calculator basic
+def cal_sth (input_string):
+    a = float(input_string[0])
+    b = float(input_string[2:])
+    anw = 0
+
+    if '+' in input_string:
+        anw = a + b
+    elif '-' in input_string:
+        anw = a - b
+    elif'x' in input_string:
+        anw = a * b
+    elif '/' in input_string:
+        anw = a/b
+
+    if anw % 10 == 0:
+        return print(int(anw))
+    else:   
+        return print(anw)
+
+print('Write like this: a_b = ')
+input_string = input()
+cal_sth(input_string)
