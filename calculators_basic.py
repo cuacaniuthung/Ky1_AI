84f07fbf358e2020eff419b2a7aeed032fe413c7
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2024-09-08 14:05:02 +0700

    calculators_basic.py

--- calculators_basic.py
+++ calculators_basic.py
@@ -1,7 +1,8 @@
 #Calculator basic
 def cal_sth (input_string):
+        
     a = float(input_string[0])
-    b = float(input_string[2:])
+    b = float(input_string[2])
     anw = 0
 
     if '+' in input_string:
@@ -12,12 +13,11 @@ def cal_sth (input_string):
         anw = a * b
     elif '/' in input_string:
         anw = a/b
-
     if anw % 10 == 0:
         return print(int(anw))
     else:   
         return print(anw)
 
-print('Write like this: a_b = ')
-input_string = input()
+print('Write like this: a _ b = ')
+input_string = list(input('a _ b = \n').split(' '))
 cal_sth(input_string)
